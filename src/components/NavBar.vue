7a6b5ac3edd2535746d1c3c5d521f1185ab329b6
Enric Bisbe Gil <enricu@gmail.com>
2017-08-27 23:55:18 +0200

    Move pool outside navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,88 +1,85 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="dark" fixed="top">
+    <div class="">
+        <b-navbar toggleable="md" type="dark" variant="dark" fixed="top">
 
-        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+            <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-        <b-navbar-brand href="#">PhotoPool</b-navbar-brand>
+            <b-navbar-brand href="#">PhotoPool</b-navbar-brand>
 
-        <b-collapse is-nav id="nav_collapse">
+            <b-collapse is-nav id="nav_collapse">
 
-            <b-nav is-nav-bar>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-nav>
+                <b-nav is-nav-bar>
+                    <b-nav-item href="#">Link</b-nav-item>
+                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                </b-nav>
 
-            <!-- Right aligned nav items -->
-            <b-nav is-nav-bar class="ml-auto">
+                <!-- Right aligned nav items -->
+                <b-nav is-nav-bar class="ml-auto">
 
-                <b-nav-item-dropdown :text="txtPool" v-if="pool.length" right>
-                    <b-dropdown-item
-                            disabled
-                            v-for="photo in pool"
-                            v-html="thumbnail(photo)"
-                            :key="photo.id"
-                    ></b-dropdown-item>
-                    <b-dropdown-divider></b-dropdown-divider>
-                    <b-dropdown-item-button
-                            size="sm"
-                            variant="danger"
-                            @click="clearPool">Clear pool
-                    </b-dropdown-item-button>
-                </b-nav-item-dropdown>
-                <b-nav-item-dropdown :text="txtPool" right v-else>
-                    <b-dropdown-item>Empty pool</b-dropdown-item>
-                </b-nav-item-dropdown>
+                    <b-nav-item @click="poolVisible = !poolVisible">{{ txtPool }}</b-nav-item>
 
-                <b-nav-item-dropdown right>
-                    <!-- Using button-content slot -->
-                    <template slot="button-content">
-                        <em>User</em>
-                    </template>
-                    <b-dropdown-item href="#">Profile</b-dropdown-item>
-                    <b-dropdown-item href="#">Signout</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-nav>
-            <b-nav-form>
-                <b-btn v-b-modal.addToPool size="sm" variant="primary">Add photos to pool</b-btn>
-            </b-nav-form>
-        </b-collapse>
+                    <b-nav-item-dropdown right>
+                        <!-- Using button-content slot -->
+                        <template slot="button-content">
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Signout</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-nav>
+                <b-nav-form>
+                    <b-btn v-b-modal.addToPool size="sm" variant="primary">Add photos to pool</b-btn>
+                </b-nav-form>
+            </b-collapse>
 
-        <b-modal id="addToPool"
-                 ref="modal1"
-                 size="lg"
-                 title="Search photos">
-            <form @submit.stop.prevent="handleSubmit">
-                <div class="form-group">
-                    <b-form-input type="text"
-                                  placeholder="Search a photo"
-                                  v-model="text"></b-form-input>
-                </div>
-                <hr>
-                <b-card-group columns v-if="photosResult">
-                    <photo
-                            v-for="photo in photosResult.photo"
-                            :photo="photo"
-                            size="n"
-                            :key="photo.nsid"
-                            selectable
-                            @selected="addToPool"
-                    ></photo>
-                </b-card-group>
-                <div v-if="pool.length">
+            <b-modal id="addToPool"
+                     ref="modal1"
+                     size="lg"
+                     title="Search photos">
+                <form @submit.stop.prevent="handleSubmit">
+                    <div class="form-group">
+                        <b-form-input type="text"
+                                      placeholder="Search a photo"
+                                      v-model="text"></b-form-input>
+                    </div>
                     <hr>
-                    <h2>Selected photos</h2>
-                    <b-card-group columns>
+                    <b-card-group columns v-if="photosResult">
                         <photo
-                                v-for="photo in pool"
+                                v-for="photo in photosResult.photo"
                                 :photo="photo"
                                 size="n"
                                 :key="photo.nsid"
+                                selectable
+                                @selected="addToPool"
                         ></photo>
                     </b-card-group>
-                </div>
-            </form>
-        </b-modal>
-    </b-navbar>
+                    <div v-if="pool.length">
+                        <hr>
+                        <h2>Selected photos</h2>
+                        <b-card-group columns>
+                            <photo
+                                    v-for="photo in pool"
+                                    :photo="photo"
+                                    size="n"
+                                    :key="photo.nsid"
+                            ></photo>
+                        </b-card-group>
+                    </div>
+                </form>
+            </b-modal>
+        </b-navbar>
+
+        <div class="pool border"
+             v-if="poolVisible">
+            <img
+                    v-for="photo in pool"
+                    :key="photo.nsid"
+                    class="img-thumbnail my-2 mx-1"
+                    :src="urlPhoto(photo)"
+                    @dblclick="addToPool(photo,false)"
+            >
+        </div>
+    </div>
 </template>
 <script>
   let _ = require('lodash')
@@ -96,7 +93,8 @@
         text: '',
         pool: [],
         photosResult: [],
-        size: 's'
+        size: 's',
+        poolVisible: false
       }
     },
     created () {
@@ -108,8 +106,9 @@
     methods: {
       addToPool (photo, add) {
         if (add) {
-          this.pool.push(photo)
+          this.pool.unshift(photo)
           this.pool = _.uniq(this.pool)
+          this.poolVisible = true
         } else {
           this.pool = _.filter(this.pool, function (o) { return o.id !== photo.id })
         }
@@ -140,3 +139,15 @@
     }
   }
 </script>
+<style>
+    .pool {
+        width: auto;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        white-space: nowrap;
+    }
+
+    .pool img:hover {
+        border-color: red;
+    }
+</style>

793d43afacd9ecc20cc15947cdbc99d22d71ad23
Enric Bisbe Gil <enricu@gmail.com>
2017-10-24 19:17:05 +0200

    Clear all

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  cognito: {
-    USER_POOL_ID: 'eu-west-1_NLiAoFoor',
-    APP_CLIENT_ID: '4e2arm7a2nmd6qg6quqdccum4l',
-    REGION: 'eu-west-1',
-    IDENTITY_POOL_ID: 'eu-west-1:5819d0a8-7d89-4f8c-94f1-6335328edcb1'
-  }
-}
-

6eb130e85a43463d43e7e5f101968978d11f0b34
Enric Bisbe Gil <enricu@gmail.com>
2017-11-05 11:00:48 +0100

    Simplify page and add center h4

--- src/router/pages/Scheduled.vue
+++ src/router/pages/Scheduled.vue
@@ -3,18 +3,53 @@
         <v-container fluid grid-list-xl>
             <v-layout row wrap>
                 <v-flex xs12>
-                    <h2 class="display-1 ma-3">Scheduled photos</h2>
-                    <scheduled-photos></scheduled-photos>
+                    <my-fetch url="/scheduled">
+                        <div slot-scope="data">
+                            <template v-for="(item, index) in scheduled(data)">
+                                <h4 class="text-xs-center mt-3" v-text="index"></h4>
+                                <v-list two-line>
+                                    <template v-for="(group, groupId) in groups(item)">
+                                        <v-subheader v-text="groupId"></v-subheader>
+                                        <template v-for="(photo, iteration) in group">
+                                            <v-divider v-show="iteration !== 0" inset></v-divider>
+                                            <photo-list :photo="photo"></photo-list>
+                                        </template>
+                                    </template>
+                                </v-list>
+                            </template>
+                        </div>
+                    </my-fetch>
                 </v-flex>
             </v-layout>
         </v-container>
     </v-content>
 </template>
 <script>
-  import ScheduledPhotos from '../../components/ScheduledPhotos.vue'
+  import PhotoList from '../../components/PhotoList.vue'
+  import * as _ from 'lodash'
+  import * as moment from 'moment'
 
   export default {
     name: 'Scheduled',
-    components: {ScheduledPhotos}
+    components: {PhotoList},
+    methods: {
+      scheduled (data) {
+        const mappedData = _.map(data, function (photo) {
+          photo.headerDate = moment(parseInt(photo.scheduledAt)).calendar(null, {
+            nextDay: '[Tomorrow]',
+            nextWeek: 'dddd, Do',
+            sameElse: 'DD-MM-YYYY',
+            lastDay: '[Yesterday]',
+            lastWeek: '[Last] dddd',
+            sameDay: '[Today]'
+          })
+          return photo
+        })
+        return _.groupBy(mappedData, 'headerDate')
+      },
+      groups (data) {
+        return _.groupBy(data, 'groupId')
+      }
+    }
   }
 </script>

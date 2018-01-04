32edac06bd928328cab46a61e22a5854599a9437
Enric Bisbe Gil <enricu@gmail.com>
2018-01-04 19:03:11 +0100

    Change home texts

--- src/router/pages/Home.vue
+++ src/router/pages/Home.vue
@@ -9,9 +9,10 @@
                         class="white--text"
                 >
                     <h1 class="white--text mb-2 display-1 text-xs-center">Improved Flickr groups workflow</h1>
-                    <div class="subheading mb-3 text-xs-center">Tell Skedr what photos add to your groups, forget the rest</div>
+                    <div class="subheading mb-3 text-xs-center">Spend less time sharing your photos, go out to take more
+                    </div>
                     <v-btn dark large class="primary lighten-2 mt-5" :to="{ path: 'groups' }">
-                        Get Started
+                        Improve my profile
                     </v-btn>
                 </v-layout>
             </v-parallax>
@@ -26,12 +27,12 @@
             >
                 <v-flex xs12 sm4 class="my-3">
                     <div class="text-xs-center">
-                        <h2 class="headline">Just do it once!</h2>
-                        <span class="subheading">Simply add one or more photos to one or more groups and forget the rest. You will get more views <v-icon>remove_red_eye</v-icon> and favs <v-icon>star</v-icon> in your photos, gaining followers in the way.
-                </span>
+                        <h2 class="headline">Share in flickr <strong>just</strong> once each photo</h2>
+                        <span class="subheading">We know it's hard to track which photo could't be added due to group restriccions. If it fails we will rember next day to add it for your, for each photo and group it missed</span>
                     </div>
                 </v-flex>
                 <v-flex xs12>
+                    <h2 class="text-xs-center">How it works?</h2>
                     <v-container grid-list-xl>
                         <v-layout row wrap align-center>
                             <v-flex xs12 md4>
@@ -40,11 +41,10 @@
                                         <v-icon x-large class="primary--text">add_to_photos</v-icon>
                                     </v-card-text>
                                     <v-card-title primary-title class="layout justify-center">
-                                        <div class="headline text-xs-center">Create a pool</div>
+                                        <div class="headline text-xs-center">Creating the pool</div>
                                     </v-card-title>
-                                    <v-card-text>Search all your photos that you want to add to the same groups. <br>Creating
-                                        a
-                                        virtual pool of images is so easy!
+                                    <v-card-text>Find all your photos that you want to share. <br>Add them to your virtual pool.
+                                        <br> You can share a single photo directly. It's even faster for single photos!
                                     </v-card-text>
                                 </v-card>
                             </v-flex>
@@ -54,10 +54,10 @@
                                         <v-icon x-large class="primary--text">share</v-icon>
                                     </v-card-text>
                                     <v-card-title primary-title class="layout justify-center">
-                                        <div class="headline">Choose groups</div>
+                                        <div class="headline">Sharing to groups</div>
                                     </v-card-title>
                                     <v-card-text>
-                                        Filter through your groups. <br>Select one, two or all of them. <br> With
+                                        Filter through your followed groups. <br>Choose one, two or all of them. <br> With
                                         <strong>one click</strong> add the pool to the selected groups.
                                     </v-card-text>
                                 </v-card>
@@ -71,7 +71,7 @@
                                         <div class="headline text-xs-center">Autoschedule photos</div>
                                     </v-card-title>
                                     <v-card-text>
-                                        All photos that cannot be added will be scheduled for next days. Take your time!
+                                        All photos that cannot be added due to group restriccions <br>will be scheduled. You will don't need to try it again next day or week.
                                     </v-card-text>
                                 </v-card>
                             </v-flex>
@@ -84,16 +84,15 @@
         <section>
             <v-parallax src="/static/img/section.jpeg" height="380">
                 <v-layout column align-center justify-center>
-                    <div class="headline white--text mb-3 text-xs-center">Improving your profile has never been so
-                        easy!
-                    </div>
-                    <em>Kick-start your application today</em>
+                    <div class="headline white--text mb-3 text-xs-center">Improve your profile</div>
+                    <em>Sharing photos to more groups will make your photos more visible at Flickr. You will gain more views and favs, followers too!
+                    </em>
                     <v-btn large
                            class="mt-5"
                            color="primary"
                            :to="{ path: 'groups' }"
                     >
-                        Get Started
+                        Sign me in!
                     </v-btn>
                 </v-layout>
             </v-parallax>

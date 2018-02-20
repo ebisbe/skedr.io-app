6670168525d2bb2236773b27f97cda7f782e3984
Enric Bisbe Gil <enricu@gmail.com>
2018-02-20 19:11:57 +0100

    Add robots.txt file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -98,6 +98,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../src/robots.txt'),
+        to: config.build.assetsRoot
       }
     ]),
     // service worker caching

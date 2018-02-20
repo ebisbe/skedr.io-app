97331454b8a605ad12ee72ac5ef53f59904c133f
Enric Bisbe Gil <enricu@gmail.com>
2018-02-20 21:00:51 +0100

    Low size of momentjs bundle

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,6 +11,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 var loadMinified = require('./load-minified')
+var ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -111,7 +112,8 @@ var webpackConfig = merge(baseWebpackConfig, {
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
       stripPrefix: 'dist/'
-    })
+    }),
+    new ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
   ]
 })
 

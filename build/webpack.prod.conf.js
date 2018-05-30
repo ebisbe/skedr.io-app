7adfd54070a937821b12346ae593e184e47cd546
Enric Bisbe Gil <enricu@gmail.com>
2018-05-30 19:15:30 +0200

    Remove SSR plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -121,12 +121,6 @@ var webpackConfig = merge(baseWebpackConfig, {
       ]
     }),
     new ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
-    // new PrerenderSPAPlugin({
-    //   // Required - The path to the webpack-outputted app to prerender.
-    //   staticDir: path.join(__dirname, '../dist'),
-    //   // Required - Routes to render.
-    //   routes: ['/', '/login/', '/signup/'],
-    // })
   ]
 })
 

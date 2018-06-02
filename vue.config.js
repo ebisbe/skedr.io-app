063f2f9d20eecdeb190c0399a2e279c23b44f1c0
Enric Bisbe Gil <enricu@gmail.com>
2018-06-02 12:17:00 +0200

    WIP BundleAnalyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const plugins = [new ContextReplacementPlugin(/moment[/\\]locale$/, /en/)]
+if (process.env.npm_config_report) {
+  plugins.push(new BundleAnalyzerPlugin())
+}
+
 module.exports = {
   chainWebpack: config => {
     config.module
@@ -5,5 +13,8 @@ module.exports = {
       .test(/\.(graphql|gql)$/)
       .use('graphql-tag')
       .loader('graphql-tag/loader')
+  },
+  configureWebpack: {
+    plugins
   }
 }

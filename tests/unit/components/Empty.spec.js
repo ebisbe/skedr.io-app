41d40aa17f9d43e8f3d3d5b5b8e7328a952e2c3d
Enric Bisbe Gil <enricu@gmail.com>
2018-06-17 11:45:11 +0200

    Check default properties on Empty.vue

--- tests/unit/components/Empty.spec.js
+++ tests/unit/components/Empty.spec.js
@@ -8,59 +8,61 @@ describe('Empty.vue', () => {
     localVue.use(Vuetify)
   })
 
-  it('it shows error with custom error text', () => {
-    const propsData = {
-      error: true,
-      description: 'Custom error text'
-    }
+  const createCmp = propsData => mount(comp, { localVue, propsData })
 
-    const wrapper = mount(comp, {
-      localVue,
-      propsData
+  describe('Properties', () => {
+    it('has default properties', () => {
+      const cmp = createCmp({})
+
+      const { icon, description, loading, error } = cmp.props()
+      expect(icon).toBe('')
+      expect(description).toBe('')
+      expect(loading).toEqual(false)
+      expect(error).toEqual(false)
     })
-    expect(wrapper.text()).toContain('Custom error text')
   })
 
-  it('it shows error with default error text', () => {
-    const propsData = {
-      error: true
-    }
+  describe('Renders texts', () => {
+    it('it shows error with custom error text', () => {
+      const propsData = {
+        error: true,
+        description: 'Custom error text'
+      }
 
-    const wrapper = mount(comp, {
-      localVue,
-      propsData
-    })
-    expect(wrapper.text()).toContain('Ups! Some error happened fetching your data...')
-  })
-
-  it('it shows message with text_icon', () => {
-    const propsData = {
-      description: 'Custom text',
-      icon: 'some_icon'
-    }
-
-    const wrapper = mount(comp, {
-      localVue,
-      propsData
+      const wrapper = createCmp(propsData)
+      expect(wrapper.text()).toContain('Custom error text')
     })
 
-    expect(wrapper.text()).toContain('Custom text')
-    expect(wrapper.text()).toContain('some_icon')
-  })
+    it('it shows error with default error text', () => {
+      const propsData = {
+        error: true
+      }
 
-  it('it shows indeterminate loader', () => {
-    const propsData = {
-      loading: true,
-      description: 'Custom text',
-      icon: 'some_icon'
-    }
-
-    const wrapper = mount(comp, {
-      localVue,
-      propsData
+      const wrapper = createCmp(propsData)
+      expect(wrapper.text()).toContain('Ups! Some error happened fetching your data...')
     })
 
-    expect(wrapper.text()).not.toContain('Custom text')
-    expect(wrapper.text()).not.toContain('some_icon')
+    it('it shows message with text_icon', () => {
+      const propsData = {
+        description: 'Custom text',
+        icon: 'some_icon'
+      }
+
+      const wrapper = createCmp(propsData)
+      expect(wrapper.text()).toContain('Custom text')
+      expect(wrapper.text()).toContain('some_icon')
+    })
+
+    it('it shows indeterminate loader', () => {
+      const propsData = {
+        loading: true,
+        description: 'Custom text',
+        icon: 'some_icon'
+      }
+
+      const wrapper = createCmp(propsData)
+      expect(wrapper.text()).not.toContain('Custom text')
+      expect(wrapper.text()).not.toContain('some_icon')
+    })
   })
 })

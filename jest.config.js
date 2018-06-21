4ed1923bc4e561ab61aa654e635aa86d8eb4e45e
Enric Bisbe Gil <enricu@gmail.com>
2018-06-21 22:19:37 +0200

    Add coverage status in README.md

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'],
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**']
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!**/node_modules/**']
 }

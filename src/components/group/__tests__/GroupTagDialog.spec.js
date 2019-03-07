fdc46f94fb74fe7e555927482deac6d0ddb2a6ec
Enric Bisbe Gil <enricu@gmail.com>
2019-03-07 18:49:24 +0100

    Test method

---
+++ src/components/group/__tests__/GroupTagDialog.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from '@vue/test-utils'
+import component from '../GroupTagDialog'
+import Vuetify from 'vuetify'
+import Vue from 'vue'
+
+Vue.use(Vuetify)
+const createComp = propsData => shallowMount(component, { propsData, stubs: ['ApolloQuery', 'ApolloMutation'] })
+describe('GroupTagDialog component', () => {
+  describe('Computed properties', () => {
+    it('has no new tag', () => {
+      const wrapper = createComp({ title: '', groupId: '123@N08', tags: ['firstTag'], manageTags: true })
+      expect(wrapper.vm.canSave).toBe(false)
+    })
+    it('adds a tag', () => {
+      const wrapper = createComp({ title: '', groupId: '123@N08', tags: ['firstTag'], manageTags: true })
+      wrapper.vm.comboTags.push('something')
+      expect(wrapper.vm.canSave).toBe(true)
+    })
+    it('adds a new tag and deletes the previous one', () => {
+      const wrapper = createComp({ title: '', groupId: '123@N08', tags: ['firstTag'], manageTags: true })
+      wrapper.vm.comboTags = ['secondTag']
+      expect(wrapper.vm.canSave).toBe(true)
+    })
+    it('deletes all tags from list', () => {
+      const wrapper = createComp({ title: '', groupId: '123@N08', tags: ['firstTag'], manageTags: true })
+      wrapper.vm.comboTags = []
+      expect(wrapper.vm.canSave).toBe(true)
+    })
+  })
+  describe('Methods', () => {
+    it('removes a tag from the comboTags', () => {
+      const wrapper = createComp({ title: '', groupId: '123@N08', tags: ['firstTag'], manageTags: true })
+      wrapper.vm.remove('firstTag')
+      expect(wrapper.vm.comboTags).toMatchObject([])
+    })
+  })
+})

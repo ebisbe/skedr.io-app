76efd663cc3944fb1ff7dccc355b582fdb31312f
Enric Bisbe Gil <enricu@gmail.com>
2019-03-29 16:26:26 +0100

    Initialize always Sentry and add rootHostname to Log Rocket

--- src/plugins/errorLoggin.js
+++ src/plugins/errorLoggin.js
@@ -2,17 +2,18 @@ import Vue from 'vue'
 import LogRocket from 'logrocket'
 import * as Sentry from '@sentry/browser'
 
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY,
+  integrations: [
+    new Sentry.Integrations.Vue({
+      Vue,
+      attachProps: true
+    })
+  ]
+})
+
 if (process.env.NODE_ENV === 'production') {
-  LogRocket.init('zye3nm/skedrio-prod')
-  Sentry.init({
-    dsn: process.env.VUE_APP_SENTRY,
-    integrations: [
-      new Sentry.Integrations.Vue({
-        Vue,
-        attachProps: true
-      })
-    ]
-  })
+  LogRocket.init('zye3nm/skedrio-prod', { rootHostname: 'skedr.io' })
   Sentry.configureScope(scope => {
     scope.addEventProcessor(async event => {
       event.extra.sessionURL = LogRocket.recordingURL

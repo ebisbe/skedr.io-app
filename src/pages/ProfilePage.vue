6f15a0d2654b4d3921a934cb17de0794eecdbfab
Enric Bisbe Gil <enricu@gmail.com>
2019-07-26 18:36:33 +0200

    Let the user choose a plan

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -13,27 +13,66 @@
               <h1 v-t="'Profile.title'"/>
               <div v-if="data && data.userSubscription && data.userSubscription.status">
                 <p v-t="'Profile.subscribed'"/>
-              </div>
-              <div v-else>
-                <p v-t="'Profile.not_subscribed'"/>
-              </div>
-              <div v-if="data && data.plans">
-                <h2 v-t="'Profile.subtitle'"/>
-                <v-list>
-                  <v-list-tile
-                    v-for="plan in data.plans"
-                    :key="plan.id">
-                    <v-list-tile-content>
-                      <v-list-tile-title>{{ plan.nickname }}: {{ parseInt(plan.amount) / 100 }}&euro; </v-list-tile-title>
-                      <v-list-tile-sub-title>{{ $tc('Profile.plan_info', plan.interval, {period: plan.interval, count: plan.interval_count, trialDays: plan.trial_period_days}) }}</v-list-tile-sub-title>
-                    </v-list-tile-content>
+                <div v-if="data.userSubscription.planId">
+                  <h2 v-t="'Profile.subtitle'"/>
+                  <v-list>
+                    <template
+                      v-for="plan in data.plans.filter( ({ id }) => id === data.userSubscription.planId)"
+                    >
+                      <v-list-tile
+                        :key="plan.id"
+                        ripple
+                        @click="selectedPlan = plan.id">
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{ plan.nickname }}: {{ parseInt(plan.amount) / 100 }}&euro; </v-list-tile-title>
+                          <v-list-tile-sub-title>{{ $tc('Profile.plan_info', plan.interval, {period: plan.interval, count: plan.interval_count, trialDays: plan.trial_period_days}) }}</v-list-tile-sub-title>
+                        </v-list-tile-content>
 
-                    <v-list-tile-action>
-                      <v-icon color="green">check</v-icon>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                </v-list>
+                        <v-list-tile-action>
+                          <v-btn icon @click="selectedPlan = plan.id">
+                            <v-icon v-if="selectedPlan === plan.id" color="green">check</v-icon>
+                            <v-icon v-else>check</v-icon>
+                          </v-btn>
+                        </v-list-tile-action>
+                      </v-list-tile>
+                    </template>
+                  </v-list>
+                </div>
               </div>
+              <template v-else>
+                <div>
+                  <p v-t="'Profile.not_subscribed'"/>
+                </div>
+                <div v-if="data && data.plans">
+                  <h2 v-t="'Profile.subtitle'"/>
+                  <v-list>
+                    <template
+                      v-for="(plan, index) in data.plans"
+                    >
+                      <v-list-tile
+                        :key="plan.id"
+                        ripple
+                        @click="selectedPlan = plan.id">
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{ plan.nickname }}: {{ parseInt(plan.amount) / 100 }}&euro; </v-list-tile-title>
+                          <v-list-tile-sub-title>{{ $tc('Profile.plan_info', plan.interval, {period: plan.interval, count: plan.interval_count, trialDays: plan.trial_period_days}) }}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+
+                        <v-list-tile-action>
+                          <v-btn icon @click="selectedPlan = plan.id">
+                            <v-icon v-if="selectedPlan === plan.id" color="green">check</v-icon>
+                            <v-icon v-else>check</v-icon>
+                          </v-btn>
+                        </v-list-tile-action>
+                      </v-list-tile>
+                      <v-divider
+                        v-if="index + 1 < data.plans.length"
+                        :key="index"
+                      />
+                    </template>
+                  </v-list>
+                </div>
+              </template>
               <div v-if="data && data.userSubscription.status === false">
                 <v-form>
                   <h2 v-t="'Profile.payment_details'"/>
@@ -53,7 +92,7 @@
                       offset-sm1>
                       <v-btn
                         v-t="'btn.submit_payment'"
-                        :disabled="!complete"
+                        :disabled="!complete || !selectedPlan"
                         :loading="loading"
                         block
                         color="primary"
@@ -95,6 +134,7 @@ export default {
     stripe: process.env.VUE_APP_STRIPE_KEY,
     complete: false,
     loading: false,
+    selectedPlan: null,
     stripeOptions: {
       base: {
         color: '#303238',
@@ -141,7 +181,8 @@ export default {
         variables: {
           name: `${this.user.name} ${this.user.family_name}`,
           email: this.user.email,
-          source: data.token.id
+          source: data.token.id,
+          planId: this.selectedPlan
         }
       })
 

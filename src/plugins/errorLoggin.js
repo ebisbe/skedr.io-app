43bf74fb050b56a2935bd48640c58b84dd92e939
Enric Bisbe Gil <enricu@gmail.com>
2020-12-03 19:13:19 +0100

    Update Sentry version

--- src/plugins/errorLoggin.js
+++ src/plugins/errorLoggin.js
@@ -1,27 +1,19 @@
 import Vue from 'vue'
 import LogRocket from 'logrocket'
-import * as Sentry from '@sentry/browser'
+import * as Sentry from '@sentry/vue'
 
-const { NODE_ENV, VUE_APP_REVISION, VUE_APP_SENTRY } = process.env
+const { VUE_APP_REVISION, VUE_APP_SENTRY } = process.env
 
-if (NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: VUE_APP_SENTRY,
-    release: VUE_APP_REVISION,
-    integrations: [
-      new Sentry.Integrations.Vue({
-        Vue,
-        attachProps: true
-      }),
-      new Sentry.Integrations.RewriteFrames()
-    ]
+Sentry.init({
+  Vue: Vue,
+  dsn: VUE_APP_SENTRY,
+  release: VUE_APP_REVISION ? VUE_APP_REVISION : 'local'
+})
+
+LogRocket.init('zye3nm/skedrio-prod', { rootHostname: 'skedr.io' })
+Sentry.configureScope(scope => {
+  scope.addEventProcessor(async event => {
+    event.extra.sessionURL = LogRocket.recordingURL
+    return event
   })
-
-  LogRocket.init('zye3nm/skedrio-prod', { rootHostname: 'skedr.io' })
-  Sentry.configureScope(scope => {
-    scope.addEventProcessor(async event => {
-      event.extra.sessionURL = LogRocket.recordingURL
-      return event
-    })
-  })
-}
+})

28733c2197f6167283c5419f137b915dab169bce
Enric Bisbe Gil <enricu@gmail.com>
2021-03-11 20:33:39 +0100

    Block suggested tags if you have no active acc

--- src/components/groupTag/GroupTagList.vue
+++ src/components/groupTag/GroupTagList.vue
@@ -48,63 +48,84 @@
               </v-chip>
             </v-flex>
             <v-flex v-else-if="group.isMember">
-              <v-menu v-model="suggestedTagsMenu" bottom offset-y max-height="300" max-width="285">
-                <template v-slot:activator="data">
-                  <v-btn
-                    v-show="hover || suggestedTagsMenu"
-                    :loading="$apolloData.queries.suggestedTags.loading"
-                    text
-                    x-small
-                    outlined
-                    @click.stop="getSuggestTagsData"
-                  >
-                    {{ $t('GroupTag.suggest_tags') }}
-                  </v-btn>
-                </template>
-                <div>
-                  <v-list v-if="suggestedTags && suggestedTags.length">
-                    <ApolloMutation
-                      v-for="tag in suggestedTags"
-                      :key="tag.name"
-                      :mutation="require('@/graphql/mutations/updateGroupTagsList.gql')"
-                      :variables="{
-                        groupId: group.id,
-                        tags: [tag.name],
-                        preventTrigger: true,
-                        useAnd: false
-                      }"
-                      tag=""
-                      @done="$emit('close')"
+              <check-subscription
+                v-slot:default="{ data: { loading: loadingSubscription, noAccess } }"
+              >
+                <v-btn
+                  v-if="noAccess"
+                  v-show="hover"
+                  :to="{ name: 'Profile' }"
+                  color="primary"
+                  x-small
+                >
+                  Support skedr
+                </v-btn>
+                <v-menu
+                  v-else
+                  v-model="suggestedTagsMenu"
+                  bottom
+                  offset-y
+                  max-height="300"
+                  max-width="285"
+                >
+                  <template v-slot:activator="data">
+                    <v-btn
+                      v-show="hover || suggestedTagsMenu"
+                      :loading="$apolloData.queries.suggestedTags.loading"
+                      :disabled="loadingSubscription"
+                      text
+                      x-small
+                      outlined
+                      @click.stop="getSuggestTagsData"
                     >
-                      <template slot-scope="{ mutate, loading, error }">
-                        <v-list-item v-if="error">
-                          <v-list-item-title v-t="'GroupTag.error_saving_tag'" />
-                        </v-list-item>
-                        <v-list-item v-else @click="mutate()">
-                          <v-list-item-avatar><v-icon>label</v-icon></v-list-item-avatar>
-                          <v-list-item-title>{{ tag.name }}</v-list-item-title>
-                          <v-list-item-action>
-                            <v-list-item-action-text>
-                              {{ parseInt((tag.count / tag.total) * 100) }}%
-                            </v-list-item-action-text>
-                            <v-list-item-action-text>
-                              {{ tag.count }} / {{ tag.total }}
-                            </v-list-item-action-text>
-                          </v-list-item-action>
-                        </v-list-item>
-                      </template>
-                    </ApolloMutation>
-                  </v-list>
-                  <v-list v-else>
-                    <v-list-item>
-                      <v-list-item-content>
-                        <v-list-item-title v-t="'GroupTag.not_enought_data'" />
-                        <v-list-item-subtitle v-t="'GroupTag.not_enought_data_suggestion'" />
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-list>
-                </div>
-              </v-menu>
+                      {{ $t('GroupTag.suggest_tags') }}
+                    </v-btn>
+                  </template>
+                  <div>
+                    <v-list v-if="suggestedTags && suggestedTags.length">
+                      <ApolloMutation
+                        v-for="tag in suggestedTags"
+                        :key="tag.name"
+                        :mutation="require('@/graphql/mutations/updateGroupTagsList.gql')"
+                        :variables="{
+                          groupId: group.id,
+                          tags: [tag.name],
+                          preventTrigger: true,
+                          useAnd: false
+                        }"
+                        tag=""
+                        @done="$emit('close')"
+                      >
+                        <template slot-scope="{ mutate, loading, error }">
+                          <v-list-item v-if="error">
+                            <v-list-item-title v-t="'GroupTag.error_saving_tag'" />
+                          </v-list-item>
+                          <v-list-item v-else @click="mutate()">
+                            <v-list-item-avatar><v-icon>label</v-icon></v-list-item-avatar>
+                            <v-list-item-title>{{ tag.name }}</v-list-item-title>
+                            <v-list-item-action>
+                              <v-list-item-action-text>
+                                {{ parseInt((tag.count / tag.total) * 100) }}%
+                              </v-list-item-action-text>
+                              <v-list-item-action-text>
+                                {{ tag.count }} / {{ tag.total }}
+                              </v-list-item-action-text>
+                            </v-list-item-action>
+                          </v-list-item>
+                        </template>
+                      </ApolloMutation>
+                    </v-list>
+                    <v-list v-else>
+                      <v-list-item>
+                        <v-list-item-content>
+                          <v-list-item-title v-t="'GroupTag.not_enought_data'" />
+                          <v-list-item-subtitle v-t="'GroupTag.not_enought_data_suggestion'" />
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                  </div>
+                </v-menu>
+              </check-subscription>
             </v-flex>
           </v-layout>
         </v-list-item-subtitle>
@@ -136,11 +157,12 @@
   import { throttleText, filters } from '@/mixins'
   import GroupTagDialog from '@/components/groupTag/GroupTagDialog'
   import PhotoLimitOptOutMessage from '@/components/groupTag/PhotoLimitOptOutMessage'
+  import CheckSubscription from '@/components/common/CheckSubscription'
   import { mapState } from 'vuex'
   import { clearTimeout } from 'timers'
 
   export default {
-    components: { GroupTagDialog, PhotoLimitOptOutMessage },
+    components: { GroupTagDialog, PhotoLimitOptOutMessage, CheckSubscription },
     mixins: [throttleText, filters],
     props: {
       groupTag: {
